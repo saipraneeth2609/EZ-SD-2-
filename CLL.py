e04637726f19583fad83321c47543de2b9bfac37
saipraneeth2609 <108877943+saipraneeth2609@users.noreply.github.com>
2023-06-24 11:49:08 +0530

    Update CLL.py

--- CLL.py
+++ CLL.py
@@ -1,31 +1,25 @@
 class Node:
-    def _init_(self,data):
-
+    def init(self,data):
         self.data=data
         self.next=None
-
-#node=Node(10)
-#print(node)  #<_main_.Node object at 0x00000179E4C47D50>
-#print(node.data) #10
-#print(node.next) #None
-
-class CLL:
         
-    def _init_(self):
+class cll:
+    def init(self):
         self.head=None
         self.tail=None
-
+        
     def display(self):
-         if self.head is None:
+        if self.head is None:
             print("Linked List is empty")
-         else:
+        else:
             temp=self.head
-            while temp.next!=self.head:
+            print(temp.data,"-->",end=" ")
+            while temp.next is not self.head:
                 temp=temp.next
-                print(temp.data)
-         print(temp.next.data)
+                print(temp.data,"-->",end=" ")
+            print(temp.next.data)
 
-    def insert_Begining(self,data):
+    def insert_begin(self,data):
         new_node=Node(data)
         if self.head is None:
             self.head=new_node
@@ -35,38 +29,17 @@ class CLL:
             new_node.next=self.head
             self.head=new_node
             self.tail.next=self.head
-        
+
     def insert_end(self,data):
+        temp=self.head
         new_node=Node(data)
-        if self.head is None:
-            self.head=new_node
-            self.tail=new_node
-            self.tail.next=self.head
-        else:
-            self.tail.next=new_node
-            self.tail=new_node
-            self.tail.next=self.head
+        while(temp!=self.tail):
+            temp=temp.next
+        temp.next=new_node
+        new_node.next=self.head
+        self.tail=new_node
 
-    def delete_begining(self):
-        if self.head is None:
-            print("link not exist")
-        else:
-            temp=self.head
-            self.head=temp.next
-            temp.next=None
-            self.tail.next=self.head
-
-    def delete_end(self):
-        temp1=self.head.next
-        prev=self.head
-        while temp1!=self.head:
-            temp1=self.head.next
-            prev=self.head
-        temp1.next=None
-        self.tail=prev
-        self.tail.next=self.head
-
-    def insert_pos(self,data,pos):
+    def insert_mid(self,pos,data):
         new_node=Node(data)
         if self.head is None:
             self.head=new_node
@@ -74,90 +47,75 @@ class CLL:
             self.tail.next=self.head
         else:
             if pos==1:
-                insert_Begining(data)
+                insert_begin(data)
             else:
                 temp=self.head
                 for i in range(1,pos-1):
                     temp=temp.next
                 new_node.next=temp.next
                 temp.next=new_node
+            
+                
+    def delete_begin(self):
+        temp=self.head
+        while(temp!=self.tail):
+            temp=temp.next
+        temp.next=self.head.next
+        self.head=self.head.next
 
-    def delete_pos(self,pos):
+    def delete_end(self):
+        temp=self.head
+        while(temp!=self.tail):
+            temp=temp.next
+        self.tail=temp
+        temp.next=self.head
+
+    def delete_mid(self,pos):
         if self.head is None:
-            print("CLL is Empty!")
+            print("CLL is empty")
         elif pos==1:
             delete_end()
         else:
             temp=self.head.next
-            prev=selself.head
+            prev=self.head
             for i in range(1,pos-1):
                 temp=temp.next
                 prev=prev.next
             prev.next=temp.next
-            
-                
-            
-        
-         
-            
 
-         
+cl=cll()
 
+node_1=Node(10)
+cl.head=node_1
 
+node_2=Node(20)
+node_1.next=node_2
 
+node_3=Node(30)
+node_2.next=node_3
 
+node_4=Node(40)
+node_3.next=node_4
+cl.tail=node_4
 
-node1=Node(10)
-node2=Node(20)
-node3=Node(30)
-node4=Node(40)
-node5=Node(50)
-cl=CLL()
-cl.head=node1
-node1.next=node2
-node2.next=node3
-node3.next=node4
-node4.next=node5
-cl.tail=node5
 cl.tail.next=cl.head
 
-print()
-print("----Insert Begin-----")
-cl.insert_Begining(9)
 cl.display()
-print()
-print("----Insert End-------")
+
+cl.insert_begin(50)
+cl.display()
+
 cl.insert_end(60)
 cl.display()
-print()
-print("----Delete Begin-----")
-cl.delete_begining()
+
+cl.insert_mid(5,70)
 cl.display()
-print()
-print("----Delete End-------")
+
+cl.delete_begin()
+cl.display()
+
 cl.delete_end()
 cl.display()
-print()
-print("----insert_position---")
-cl.insert_pos(40,3)
-cl.display()
-print()
-print("----delete_pos--------")
-cl.delete_pos(3)
-cl.display()
 
-
-'''print(node1)
-print(node1.data)
-print(node1.next)
-print()
-print(node2)
-print(node2.data)
-print(node2.next)
-print()
-print(node3)
-print(node3.data)
-print(node3.next)
-print(node4)
-print(node4.data)
-print(node4.next)''
+cl.delete_mid(4)
+cl.display()
